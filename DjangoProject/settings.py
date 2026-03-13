72a63818261731cb0f3098bcb405f50ea1802f29
Porphyrios <148293084+SaulyD2025@users.noreply.github.com>
2026-03-13 14:49:28 +0000

    Changed debug settings

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(tcdqjodr#-+d9be#^oc=!6u6wfzh!qorbuo_hf_0&g#g=+1(g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tasports.onrender.com']
 
